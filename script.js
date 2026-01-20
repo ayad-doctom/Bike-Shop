c59f89522bed8b07d7b4e2db868e979e2cf7c10f
Ayadi-docker <ayadimotos@gmail.com>
2026-01-20 20:10:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -54,3 +54,36 @@ document.getElementById("checkout-form").addEventListener("submit", function(e) 
   alert("Order placed successfully!");
   clearCart();
 });
+function sendWhatsApp() {
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var message = document.getElementById("message").value;
+
+  var whatsappNumber = "212642487482";
+
+  var text =
+    "الاسم: " + name + "%0A" +
+    "الهاتف: " + phone + "%0A" +
+    "الرسالة: " + message;
+
+  var url = "https://wa.me/" + whatsappNumber + "?text=" + text;
+
+  window.open(url, "_blank");
+}
+function sendEmail() {
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var message = document.getElementById("message").value;
+ // بدلي الإيميل ديالك
+
+  var subject = "رسالة جديدة من الموقع";
+  var body =
+    "الاسم: " + name + "\n" +
+    "الهاتف: " + phone + "\n\n" +
+    "الرسالة:\n" + message;
+
+  window.location.href =
+    "mailto:" + email +
+    "?subject=" + encodeURIComponent(subject) +
+    "&body=" + encodeURIComponent(body);
+}
